d55363c5ebc9f6f70498eb5ea1a720d58eb02977
anythiinq <108547358+anythiinq@users.noreply.github.com>
2025-01-31 11:05:58 -0800

    Update Themes.java

--- Themes.java
+++ Themes.java
@@ -1,12 +1,16 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Themes {
     private static JLabel backgroundLabel;
+    private static guiImplementation gui;
 
-    public static void setPage() {
+    public Themes(guiImplementation g) {
+        gui = g;
+    }
+
+    public void launchPage() {
         JFrame page = new JFrame();
         page.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         page.setSize(1366, 768);
@@ -19,65 +23,22 @@ public class Themes {
         page.add(backgroundLabel);
 
         // buttons
-        JButton teddyButton = createTransparentButton();
-        teddyButton.setBounds(721, 210, 230, 60);
+        JButton teddyButton = createTransparentButton("theme-background.png", 721, 210, 230, 60);
         page.add(teddyButton);
-        teddyButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                changeBackground("theme-background.png");
-            }
-        });
-
-        JButton floralButton = createTransparentButton();
-        floralButton.setBounds(721, 307, 230, 60);
+        JButton floralButton = createTransparentButton("floral-theme-bg.png", 721, 307, 230, 60);
         page.add(floralButton);
-        floralButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                changeBackground("floral-theme-bg.png");
-            }
-        });
-
-        JButton bakeryButton = createTransparentButton();
-        bakeryButton.setBounds(721, 403, 230, 60);
+        JButton bakeryButton = createTransparentButton("bakery-theme-bg.png", 721, 403, 230, 60);
         page.add(bakeryButton);
-        bakeryButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                changeBackground("bakery-theme-bg.png");
-            }
-        });
-
-        JButton holidayButton = createTransparentButton();
-        holidayButton.setBounds(1020, 210, 230, 60);
+        JButton holidayButton = createTransparentButton("holiday-theme-bg.png", 1020, 210, 230, 60);
         page.add(holidayButton);
-        holidayButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                changeBackground("holiday-theme-bg.png");
-            }
-        });
-
-        JButton gothicButton = createTransparentButton();
-        gothicButton.setBounds(1020, 307, 230, 60);
+        JButton gothicButton = createTransparentButton("gothic-theme-bg.png", 1020, 307, 230, 60);
         page.add(gothicButton);
-        gothicButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                changeBackground("gothic-theme-bg.png");
-            }
-        });
-
-        JButton plantButton = createTransparentButton();
-        plantButton.setBounds(1020, 403, 230, 60);
+        JButton plantButton = createTransparentButton("plant-theme-bg.png", 1020, 403, 230, 60);
         page.add(plantButton);
-        plantButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                changeBackground("plant-theme-bg.png");
-            }
-        });
+
+        // back button
+        JButton backButton = createTransparentButton(null, 568, 555, 230, 60);
+        page.add(backButton);
 
         page.setVisible(true);
     }
@@ -86,15 +47,23 @@ public class Themes {
         backgroundLabel.setIcon(new ImageIcon(imagePath)); // update the background image
     }
 
-    public static JButton createTransparentButton() {
+    public static JButton createTransparentButton(String imagePath, int x, int y, int width, int height) {
         JButton button = new JButton();
         button.setOpaque(false);
         button.setContentAreaFilled(false);
         button.setBorderPainted(false);
-        return button;
-    }
 
-    public static void main(String[] args) {
-        setPage();
+        button.setBounds(x, y, width, height);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (imagePath.equals(null)) {
+                    gui.launchHome();
+                } else {
+                    changeBackground(imagePath);
+                }
+            }
+        });
+        return button;
     }
 }
