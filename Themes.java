6e9d1cabd424c0a42e977975bcddf497ace2b81f
anythiinq <108547358+anythiinq@users.noreply.github.com>
2025-02-06 10:33:33 -0800

    Update Themes.java

--- Themes.java
+++ Themes.java
@@ -8,39 +8,40 @@ public class Themes {
 
     public Themes(guiImplementation g) {
         gui = g;
+
+        // background label to hold the image
+        backgroundLabel = new JLabel(new ImageIcon("theme-background.png"));
+        backgroundLabel.setBounds(0, 0, 1366, 768); // set size to match the frame
     }
 
     public void launchPage() {
         // remove all components from layeredPane
         gui.getLayeredPane().removeAll();
 
-        // background label to hold the image
-        backgroundLabel = new JLabel(new ImageIcon("theme-background.png"));
-        backgroundLabel.setBounds(0, 0, 1366, 768); // set size to match the frame
         gui.getLayeredPane().add(backgroundLabel, Integer.valueOf(1));
 
         // buttons
         JButton teddyButton = createTransparentButton("theme-background.png", "homepage.png", "addEntry.png", 721, 221, 230, 60);
-        gui.getLayeredPane().add(teddyButton, Integer.valueOf(2));
+        gui.getLayeredPane().add(teddyButton);
 
         JButton floralButton = createTransparentButton("floral-theme-bg.png", "flower-homepage.png", "", 721, 319, 230, 60);
-        gui.getLayeredPane().add(floralButton, Integer.valueOf(2));
+        gui.getLayeredPane().add(floralButton);
 
-        JButton bakeryButton = createTransparentButton("bakery-theme-bg.png", "", "", 721, 417, 230, 60);
-        gui.getLayeredPane().add(bakeryButton, Integer.valueOf(2));
+        JButton bakeryButton = createTransparentButton("bakery-theme-bg.png", "bakery-homepage.png", "", 721, 417, 230, 60);
+        gui.getLayeredPane().add(bakeryButton);
 
-        JButton holidayButton = createTransparentButton("holiday-theme-bg.png", "", "", 1020, 221, 230, 60);
-        gui.getLayeredPane().add(holidayButton, Integer.valueOf(2));
+        JButton holidayButton = createTransparentButton("holiday-theme-bg.png", "holiday-homepage.png", "", 1020, 221, 230, 60);
+        gui.getLayeredPane().add(holidayButton);
 
-        JButton gothicButton = createTransparentButton("gothic-theme-bg.png", "", "", 1020, 319, 230, 60);
-        gui.getLayeredPane().add(gothicButton, Integer.valueOf(2));
+        JButton gothicButton = createTransparentButton("gothic-theme-bg.png", "gothic-homepage.png", "", 1020, 319, 230, 60);
+        gui.getLayeredPane().add(gothicButton);
 
-        JButton plantButton = createTransparentButton("plant-theme-bg.png", "", "", 1020, 417, 230, 60);
-        gui.getLayeredPane().add(plantButton, Integer.valueOf(2));
+        JButton plantButton = createTransparentButton("plant-theme-bg.png", "plant-homepage.png", "", 1020, 417, 230, 60);
+        gui.getLayeredPane().add(plantButton);
 
         // back button to return to home page
         JButton backButton = createTransparentButton(null, null, null, 568, 555, 230, 60);
-        gui.getLayeredPane().add(backButton, Integer.valueOf(2));
+        gui.getLayeredPane().add(backButton);
 
         // refresh the frame to display the updated panel
         gui.getLayeredPane().revalidate();
