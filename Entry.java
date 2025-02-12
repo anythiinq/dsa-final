d684af9f287fd691962630a5edafee41e39babde
anythiinq <108547358+anythiinq@users.noreply.github.com>
2025-02-11 20:05:25 -0800

    Update Entry.java

--- Entry.java
+++ Entry.java
@@ -2,12 +2,18 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+ * Purpose: stores user’s mood and journal entry for that day. It also has getter and setter methods so that the user can edit their entries for that day and it will be saved/ updated in the back-end
+ * 
+ * Author: Sahiti Bulusu, Krystal Sun, Joy Wang
+ */
 public class Entry {
     private String mood;
     private String content;
     private String date;
     private static final Map<String, String> MOOD_COLORS;
 
+    // Maps each mood to a color for easy visualization in the calendar layout page (color coding)
     static {
         MOOD_COLORS = new HashMap<>();
         MOOD_COLORS.put("anxious", "blue");
@@ -16,6 +22,7 @@ public class Entry {
         MOOD_COLORS.put("ennui", "purple");
     }
 
+    // constructor
     public Entry(String mood, String content) {
         if (!MOOD_COLORS.containsKey(mood)) {
             throw new IllegalArgumentException("Invalid mood. Please select a valid mood: " + MOOD_COLORS.keySet());
@@ -25,29 +32,34 @@ public class Entry {
         this.date = LocalDate.now().toString();
     }
 
+    // accessor method
     public String getMood() {
         return mood;
     }
 
+    // accessor method
     public String getContent() {
         return content;
     }
 
+    // accessor method
     public String getDate() {
         return date;
     }
 
+    // accessor method
     public String getMoodColor() {
         return MOOD_COLORS.get(mood);
     }
 
+    // toString method
     @Override
     public String toString() {
         return "Date: " + date + "\nMood: " + mood + " (Color: " + getMoodColor() + ")\nContent: " + content;
     }
 
+    // accessor method
     public static Map getMOODCOLORS() {
         return MOOD_COLORS;
     }
 }
-    
